900758fce27a65478e6d46e49abd6fd4cde706d9
lr1112 <luis_rosario_0324@hotmail.com>
2022-06-21 12:42:20 -0400

    creando proyecto e instalando dependencias

---
+++ app/src/main/java/edu/ucne/parcial_en_compose/MainActivity.kt
@@ -0,0 +1,43 @@
+package edu.ucne.parcial_en_compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import edu.ucne.parcial_en_compose.ui.theme.Parcial_en_composeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Parcial_en_composeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Parcial_en_composeTheme {
+        Greeting("Android")
+    }
+}
